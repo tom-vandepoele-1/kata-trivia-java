74f1283ac30e1fe581b20945acea661d13a95cad
Lukasz Fijas <lukasz.fijas@pl.bosch.com>
2023-06-05 11:47:59 +0200

    Improved resolving current category

--- src/main/java/trivia/GameBetter.java
+++ src/main/java/trivia/GameBetter.java
@@ -95,16 +95,27 @@ public class GameBetter implements IGame {
 
 
    private String currentCategory() {
-      if (places[currentPlayer] == 0) return "Pop";
-      if (places[currentPlayer] == 4) return "Pop";
-      if (places[currentPlayer] == 8) return "Pop";
-      if (places[currentPlayer] == 1) return "Science";
-      if (places[currentPlayer] == 5) return "Science";
-      if (places[currentPlayer] == 9) return "Science";
-      if (places[currentPlayer] == 2) return "Sports";
-      if (places[currentPlayer] == 6) return "Sports";
-      if (places[currentPlayer] == 10) return "Sports";
-      return "Rock";
+      int currentPlayerPlace = places[currentPlayer];
+      return provideCategory(currentPlayerPlace);
+   }
+
+   private String provideCategory(int currentPlayerPlace) {
+      switch (currentPlayerPlace) {
+         case 0:
+         case 4:
+         case 8:
+            return "Pop";
+         case 1:
+         case 5:
+         case 9:
+            return "Science";
+         case 2:
+         case 6:
+         case 10:
+            return "Sports";
+         default:
+            return "Rock";
+      }
    }
 
    public boolean wasCorrectlyAnswered() {
